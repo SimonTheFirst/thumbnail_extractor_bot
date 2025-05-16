752c4b360857adf1d1b621c7ffdec7b860536c90
Simonov <kirillsimonov@yandex.com>
2025-05-16 16:24:18 +0300

    Rework Youtube thumbnail extraction. Now it is done with YouTube API call. Refactor extractors.

--- thumbnail_extractor.py
+++ thumbnail_extractor.py
@@ -1,10 +1,28 @@
 import re
+import os
+from dataclasses import dataclass, asdict
 from typing import Protocol
 from urllib.parse import urlparse, parse_qs
 from os import getenv
+from abc import ABC, abstractmethod
 
+from googleapiclient.discovery import build
 from aiohttp import ClientSession, ClientResponseError
 
+
+@dataclass
+class YoutubeThumbnailSizes:
+    maxres: dict = None
+    standard: dict = None
+    high: dict = None
+    medium: dict = None
+    default: dict = None
+
+    def get_max_available_res(self):
+        for name, value in asdict(self).items():
+            if value:
+                return self.__getattribute__(name)['url']
+        return None
 
 class VideoDataError(Exception):
     """Исключение для пустых/битых/приватных данных видео"""
@@ -32,8 +50,7 @@ class VkThumbnailExtractor:
     """Извлекатель обложек для VK Видео."""
     API_URL = 'https://api.vk.ru/method/video.get'
 
-    @classmethod
-    def _get_video_id_from_url(cls, url: str) -> str | None:
+    def _get_video_id_from_url(self, url: str) -> str | None:
         """
         Получает id видео из url
         
@@ -56,7 +73,7 @@ class VkThumbnailExtractor:
         :raises APIResponseError: Если запрос к API вернулся с ошибкой
         :raises VideoDataError: Если возникли проблемы с данными видео
         """
-        video_id = VkThumbnailExtractor._get_video_id_from_url(url)
+        video_id = self._get_video_id_from_url(url)
 
         request_params = {
             'videos': video_id,
@@ -87,46 +104,64 @@ class VkThumbnailExtractor:
                 else:
                     raise VideoDataError(f'Не удалось получить обложки для видео с id {video_id} (пустой список).', json_data)
 
-class YoutubeThumbnailExtractor:
+class YoutubeThumbnailExtractor(ABC):
     """Извлекатель обложек для YouTube."""
 
-    @classmethod
-    def _get_video_id_from_url(cls, url: str) -> str | None:
+    @abstractmethod
+    def _get_video_id_from_url(self, url: str) -> str | None:
         """
         Извлекает id видео из url
 
         :param url: Url видео
         :raises ValueError: Если не удается извлечь id из url
         """
-        parsed_url = urlparse(url)
-        match parsed_url.netloc:
-            case 'www.youtube.com':
-                if not parsed_url.query:
-                    raise ValueError(f'В url {url} нет id видео')
-                else:
-                    return parse_qs(parsed_url.query)['v'][0]
-                
-            case 'youtu.be':
-                if not parsed_url.path:
-                    raise ValueError(f'В url {url} нет id видео')
-                else:
-                    return parsed_url.path.replace('/', '')
+        raise NotImplementedError()
 
-    # TODO заменить на обращение к API, чтобы URL не устаревал
     async def extract_thumbnail(self, url):
         """
         Извлекает обложку для видео с переданным url
         
         :param url: Url видео
         """
-        video_id = YoutubeThumbnailExtractor._get_video_id_from_url(url)
-        return f'https://i.ytimg.com/vi/{video_id}/maxresdefault.jpg'
-
+        api_client = build('youtube', 'v3', developerKey=os.getenv('YOUTUBE_API_KEY'))
+        video_id = self._get_video_id_from_url(url)
+        request = api_client.videos().list(
+            part='snippet',
+            id=video_id
+        )
+        response = request.execute()
+        items = response.get('items', [])
+        if not items:
+            raise ValueError('Видео не найдено')
+        
+        snippet = items[0]['snippet']
+        thumbnails_dict = snippet.get('thumbnails', {})
+        if not thumbnails_dict:
+            raise ValueError('Обложки не найдены')
+        
+        thumbnails = YoutubeThumbnailSizes(**thumbnails_dict)
+        return thumbnails.get_max_available_res()
+    
+class YoutubeFullUrlThumbnailExtractor(YoutubeThumbnailExtractor):
+    def _get_video_id_from_url(self, url: str) -> str | None:
+        parsed_url = urlparse(url)
+        if not parsed_url.query:
+            raise ValueError(f'В url {url} нет id видео')
+        else:
+            return parse_qs(parsed_url.query)['v'][0]
+        
+class YoutubeShortUrlThumbnailExtractor(YoutubeThumbnailExtractor):
+    def _get_video_id_from_url(self, url: str) -> str | None:
+        parsed_url = urlparse(url)
+        if not parsed_url.path:
+            raise ValueError(f'В url {url} нет id видео')
+        else:
+            return parsed_url.path.replace('/', '')
+        
 class RutubeThumbnailExtractor:
     """Извлекатель обложек для RuTube."""
 
-    @classmethod
-    def _get_video_id_from_url(cls, url: str) -> str | None:
+    def _get_video_id_from_url(self, url: str) -> str | None:
         """
         Получает id видео из url
         
@@ -147,7 +182,7 @@ class RutubeThumbnailExtractor:
         :param url: Url видео
         :raises ValueError: Если видео с переданным url не найдено
         """
-        video_id = RutubeThumbnailExtractor._get_video_id_from_url(url)
+        video_id = self._get_video_id_from_url(url)
         api_url = f'https://rutube.ru/api/video/{video_id}'
         try:
             async with ClientSession() as session:
@@ -166,8 +201,9 @@ class ThumbnailExtractorFactory():
         'vkvideo': VkThumbnailExtractor,
         'vk': VkThumbnailExtractor,
         'rutube': RutubeThumbnailExtractor,
-        'youtube': YoutubeThumbnailExtractor,
-        'youtu': YoutubeThumbnailExtractor
+        'youtube': YoutubeFullUrlThumbnailExtractor,
+        'm.youtube': YoutubeFullUrlThumbnailExtractor,
+        'youtu': YoutubeShortUrlThumbnailExtractor
     }
 
     @classmethod
